c25966d2c51be801315aa45bc25cb4b7d2602b28
Aniket <aniket@nirmata.com>
2023-06-25 19:53:41 +0530

    Initialize next project for ecommerce app

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+        <Head>
+            <title>Next Shop</title>
+        </Head>
+        <main>
+            <h1>Next Shop</h1>
+        </main>
+    </>
+  );
+}
+
+export default HomePage;
