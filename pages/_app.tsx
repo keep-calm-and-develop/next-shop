adbcf3e10bb2b5c2d7ef120c49dfc836e180262d
Aniket <aniket@nirmata.com>
2023-07-05 17:12:23 +0530

    Add react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+        </QueryClientProvider>
+    );
 }
 
 export default App;
